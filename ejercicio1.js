0ce4f9f6f996ac37ff4119bec939b0edf500c0b6
Naomi García Robles <naomigarciarobles21@gmail.com>
2024-07-10 12:41:26 +0200

    Arrow function 1.2

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,4 +1,5 @@
 // Ejercicio 1.1. Arrow functions
+
 // Nivel 1
 // Ejercicio 1
 
@@ -7,5 +8,9 @@ function add(a, b) {
 }
 //Conversion
 const suma = (a, b) => a + b;
-
 console.log(suma(2, 3));
+
+//Ejercicio 2
+
+const randomNumber = () => Math.floor(Math.random() * 101);
+console.log(randomNumber());
